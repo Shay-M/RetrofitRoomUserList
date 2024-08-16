a7522c4dce3860b2c274b4d3a2e01e50210dbb68
“ShayM” <shay1mualem@gmail.com>
2024-08-16 21:04:13 +0300

    add list user

---
+++ app/src/main/java/com/easysale/retrofitroomuserlist/ui/UserListFragment.java
@@ -0,0 +1,13 @@
+package com.easysale.retrofitroomuserlist.ui;
+
+import androidx.fragment.app.Fragment;
+
+
+public class UserListFragment extends Fragment {
+
+
+    public UserListFragment() {
+        // Required empty public constructor
+    }
+
+}
