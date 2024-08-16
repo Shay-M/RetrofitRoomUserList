df3d1cdf6bdd01d9e47d0987e25493d63f0704ff
“ShayM” <shay1mualem@gmail.com>
2024-08-16 20:21:19 +0300

    add user card

---
+++ app/src/main/java/com/easysale/retrofitroomuserlist/ui/adapter/BindingAdapters.java
@@ -0,0 +1,18 @@
+package com.easysale.retrofitroomuserlist.ui.adapter;
+
+
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.bumptech.glide.Glide;
+
+public class BindingAdapters {
+
+    @BindingAdapter("imageUrl")
+    public static void loadImage(ImageView view, String imageUrl) {
+        Glide.with(view.getContext())
+                .load(imageUrl)
+                .into(view);
+    }
+}
