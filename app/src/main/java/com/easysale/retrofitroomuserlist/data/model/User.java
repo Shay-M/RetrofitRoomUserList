7c50c674b855839f89a57232d1ac270edb272389
“ShayM” <shay1mualem@gmail.com>
2024-08-19 19:01:03 +0300

    map the JSON keys to the fields in your User class

--- app/src/main/java/com/easysale/retrofitroomuserlist/data/model/User.java
+++ app/src/main/java/com/easysale/retrofitroomuserlist/data/model/User.java
@@ -3,16 +3,24 @@ package com.easysale.retrofitroomuserlist.data.model;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import retrofit2.http.GET;
 
 @Data
 @Entity(tableName = "users")
+@AllArgsConstructor
 public class User implements Serializable {
     @PrimaryKey
     private int id;
+    @SerializedName("first_name")
     private String firstName;
+    @SerializedName("last_name")
     private String lastName;
     private String avatar;
 }
